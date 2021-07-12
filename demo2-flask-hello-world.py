5b93ead0b5ec1c325dbd769326e2f87af1763b03
pi <payam.farah@live.com>
2021-07-11 23:47:41 -0700

    success using db.create_all() to instantiate tables

--- demo2-flask-hello-world.py
+++ demo2-flask-hello-world.py
@@ -10,10 +10,22 @@ from dotenv import load_dotenv
 
 
 app = Flask(__name__)
-app.config['SQLAlCHEMY_DATABASE_URI'] = os.getenv('CONNECTION_STRING')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('CONNECTION_STRING')
 
 # links an instance of the database with the flask application
 db = SQLAlchemy(app)
+
+
+# creating a datamodel (a table) using sqlalchemy ORM
+# "By inheriting from db.Model, we map from our classes to tables via SQLAlchemy ORM"
+class Person(db.Model):
+
+    #by default it is named lowercase class name but can customize this with:
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(), nullable=False)
+
+db.create_all()
 
 @app.route('/')
 def index():
