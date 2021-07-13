75f3a6680ad30722933cdd0476e4a3532ef5a5a3
pi <payam.farah@live.com>
2021-07-13 00:19:30 -0700

    added simple form to add tasks to db

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 import os
 from dotenv import load_dotenv
 
+
+# to run:
+# FLASK_APP=app.py flask run
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('CONNECTION_STRING_TODO')
 db = SQLAlchemy(app)
 
+
+
+
+
+# this is the modesl (M)
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,8 +25,17 @@ class Todo(db.Model):
 def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
+# This is the controller (C)
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all())
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 db.create_all()
