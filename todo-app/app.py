0ac96e5fff03fdf1181d885704fbb5e64d21531b
pi <payam.farah@live.com>
2021-07-13 02:58:54 -0700

    unexpected behavior with view displaying json object on submit

--- todo-app/app.py
+++ todo-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 import os
@@ -25,6 +25,8 @@ class Todo(db.Model):
 def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
+db.create_all()
+
 # This is the controller (C)
 @app.route('/')
 def index():
@@ -32,10 +34,12 @@ def index():
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
+    description = request.form['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+        'description': todo.description
+    })
+    
 
-db.create_all()
